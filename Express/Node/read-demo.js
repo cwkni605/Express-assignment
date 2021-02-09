762e4651f95a54430e6b9c71b9edf61137c0894a
connor knight <cwkni605@gmail.com>
2021-02-08 20:32:16 -0700

    This is where I added the dependecies then express said no
    This is where I added the dependencies then express said no

---
+++ Express/Node/read-demo.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+fs.readFile('./data1.json', 'utf-8', function(err, data) {
+    console.log(data);
+    data = JSON.parse(data);
+    console.log(data.name);
+    });
---
+++ Express/Node/read-directory-demo.js
@@ -0,0 +1,4 @@
+var fs = require('fs');
+fs.readdir('c:/', function(err, data) {
+    console.log(data);
+});
